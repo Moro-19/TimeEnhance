3c7447e1546256ad39c997ff562687499594c756
moro <marwanamir2006@gmail.com>
2025-12-20 22:26:31 +0200

    Fixed captiliaztion issue to try and pass tests

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -15,7 +15,7 @@ from Models.StoreItem import StoreItem
 class TestUserController:
     @patch("Controllers.User_controller.user_repo")
     def test_login_route_get(self, mock_repo, client):
-        response = client.get('/users/login')
+        response = client.get('/users/Login')
         
         assert response.status_code == 200
     
@@ -24,7 +24,7 @@ class TestUserController:
         mock_user = User("user1", "testuser", "hash", "test@test.com", 150, 50)
         mock_repo.get_all_users.return_value = [mock_user]
         
-        response = client.get('/users/user1/profile')
+        response = client.get('/users/user1/Profile')
         
         assert response.status_code == 200
 
